0429f0861c7af50a191fa862a7b9ae434d528977
Eva Maria <eguerrerosanz@gmail.com>
2023-10-26 16:03:32 +0200

    Se crea abrir galería

--- src/eventoCategorias.js
+++ src/eventoCategorias.js
@@ -1,8 +1,10 @@
 const contenedorCategorias = document.getElementById('categorias');
+const galeria = document.getElementById('galeria');
+
 
 contenedorCategorias.addEventListener('click', (e) => {
     e.preventDefault();
     if(e.target.closest('a')) {
-        console.log('ejecuta');
+        galeria.classList.add('galeria--active');
     }
 });
