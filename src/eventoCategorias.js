609566ab6b4b6cead3c7d256ac47e62128b4a9b3
Eva Maria <eguerrerosanz@gmail.com>
2023-10-26 16:47:34 +0200

    Se elimina scroll al body

--- src/eventoCategorias.js
+++ src/eventoCategorias.js
@@ -4,7 +4,10 @@ const galeria = document.getElementById('galeria');
 
 contenedorCategorias.addEventListener('click', (e) => {
     e.preventDefault();
+
+
     if(e.target.closest('a')) {
         galeria.classList.add('galeria--active');
+        document.body.style.overflow = 'hidden';
     }
 });
